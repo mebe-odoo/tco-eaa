1fff5fc5015c142202fcb6560746440361b97505
Rathanavel Subramaniam <rathanavel@live.in>
2022-09-21 17:05:53 +0400

    updates

--- awesome_school/models/awesome_school.py
+++ awesome_school/models/awesome_school.py
@@ -1,8 +1,36 @@
+from email.policy import default
+from pydoc import describe
 from odoo import models, fields
 
-class School(models.Model):
+
+class AwesomeSchoolBase(models.Model):
     _name = 'awesome.school'
     _description = 'Awesome School'
 
-    firstname = fields.Char('First Name')
-    lastname = fields.Char('Last Name')
+    name = fields.Char('Name', required=True)
+    supervisor = fields.Char('Supervisor', required=True)
+    establishment_date = fields.Char('Establishment Date')
+    street = fields.Char('Street')
+    street_2 = fields.Char('Street 2')
+    city = fields.Char('City')
+    zip = fields.Char('Zip')
+    State = fields.Char('State')
+    country = fields.Char('Country')
+
+
+class AwesomeClassroomBase(models.Model):
+    _name = 'awesome.classroom'
+    _description = 'Awesome Classroom'
+
+    name = fields.Char('Name', required=True)
+    capacity = fields.Char('Capacity', default=10)
+    teacher = fields.Char('Teacher')
+    students = fields.Char('Students')
+
+
+# Cclass AwesomeSchoolBaseExtention(models.Model):
+#      _name = 'test.model'
+#     _inherit = ['test.model.base', 'mail.thread']
+#     _inherits = {'res.partner': 'delegated_partner_id'}
+#     _description = 'Test Model'
+#     _order = 'age ASC'
