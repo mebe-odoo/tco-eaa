81eee743b4be33010d468f6ae7233c82ca470226
Rathanavel Subramaniam <rathanavel@live.in>
2022-09-21 17:24:47 +0400

    updates

--- awesome_school/models/awesome_school.py
+++ awesome_school/models/awesome_school.py
@@ -7,15 +7,33 @@ class AwesomeSchoolBase(models.Model):
     _name = 'awesome.school'
     _description = 'Awesome School'
 
-    name = fields.Char('Name', required=True)
-    supervisor = fields.Char('Supervisor', required=True)
+    name = fields.Char('School Name', required=True)
+    # supervisor = fields.Char('Supervisor', required=True)
+
+    # supervisor_id = fields.Many2one("res.partner")
+    # teacher_id = fields.Many2one("res.partner")
+    # student_id = fields.Many2one("res.partner")
+
     establishment_date = fields.Char('Establishment Date')
     street = fields.Char('Street')
     street_2 = fields.Char('Street 2')
+    country = fields.Char('Country')
     city = fields.Char('City')
     zip = fields.Char('Zip')
     State = fields.Char('State')
-    country = fields.Char('Country')
+
+    classroom_ids = fields.Many2many(
+                "awesome.classroom",
+                "school_classroom_model_m2m_rel",
+                "school_id",
+                "classroom_id")
+    students_ids = fields.Many2many(
+                "res.partner",
+                "students_partners_model_m2m",
+                "school_id",
+                "partner_id") 
+
+
 
 
 class AwesomeClassroomBase(models.Model):
